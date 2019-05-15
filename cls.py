8b9845f713f5b8081a421e17b51df2b435d17be3
SM P <zzererer0471@gmail.com>
2019-05-16 00:47:45 +0900

    190515 commit cls   - pretrained 사용여부 옵션추가   - torch.backends.benchmark=True 추가   - nn.DataParallel 공통라인 위로 뺌(train, test 둘 다 적용하게)   - model.train, model.eval 추가

--- cls.py
+++ cls.py
@@ -16,21 +16,35 @@ def build_model(opt, phase, device):
 
     # model=gradual_cls(opt.sample_duration,opt.sample_size,opt.sample_size,model,num_classes)
     print(model)
-    if phase == 'train' and opt.pretrain_path:
-        model.load_weights(opt.pretrain_path)
+    if not opt.no_pretrained_model:
+        print("use pretrained model")
+        if phase == 'train' and opt.pretrain_path:
+            model.load_weights(opt.pretrain_path)
+    else:
+        print("no pretrained model")
 
     # `19.3.8
-    model = model.cuda(device)
-    # model.to(device)
+    # model = model.cuda(device)
+    if not opt.no_cuda:
+        torch.backends.benchmark = True
+        model.to(device)
+        # model.cuda()
 
+    # `19.5.14.
+    # use multi_gpu for training and testing
+    model = nn.DataParallel(model, device_ids=range(opt.gpu_num))
     if phase == 'train':
         # for debug
         # print(opt.gpu_num)
 
         # `19.3.8
         # I'll use only one gpu
-        model = nn.DataParallel(model, device_ids=range(opt.gpu_num))
+        # model = nn.DataParallel(model, device_ids=range(1))
+        # use multi gpu for only training
+        # model = nn.DataParallel(model, device_ids=range(opt.gpu_num))
+        model.train()
     else:
-        model = nn.DataParallel(model, device_ids=range(1))
+        # model = nn.DataParallel(model, device_ids=range(1))
+        model.eval()
 
     return model
