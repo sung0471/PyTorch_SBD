560476e7ed53ca95ec105c4a1f1ce4f5ab71d041
SM P <zzererer0471@gmail.com>
2019-05-16 00:34:53 +0900

    190515 commit models/__init__   - resnext 부분 추가 models/resnet   - forward에서 .to(device) 주석처리   - main으로 실행할 때 동작할 명령어 추가   - multibox를 위한 함수 제작 중-- models/resnext   - resnext 코드 추가   - 원 코드에서 없는 load_weight 함수 resnet에서 가져옴 models/squeezenet   - squeezenet.cuda() > squeezenet.to(device) 수정   - x.cuda() 부분 분리 > 나중에 주석처리 models/teacher_student_net   - teacher, student 모델로 증류학습하는 모델 추가   - x.to(self.device) 추가 > 나중에 주석처리

---
+++ models/teacher_student_net.py
@@ -0,0 +1,45 @@
+from models.deepSBD import *
+from opts import parse_opts
+from cls import build_model
+import os
+
+
+class teacher_student_net(nn.Module):
+    def __init__(self, opt, teacher_model_path, phase, device):
+        self.phase = phase
+        self.device = device
+        super(teacher_student_net, self).__init__()
+
+        opt.model = 'alexnet'
+        self.teacher_model = build_model(opt, 'test', device)
+        self.teacher_model_path = teacher_model_path
+        self.load_checkpoint(self.teacher_model, self.teacher_model_path)
+
+        opt.model = 'resnext'
+        self.student_model = build_model(opt, 'train', device)
+
+    def forward(self, x):
+        # x = x.to(self.device)
+        # x = x.cuda()
+        if self.phase == 'train':
+            teacher_x = self.teacher_model(x)
+            student_x = self.student_model(x)
+            out = (teacher_x, student_x)
+        else:
+            student_x = self.student_model(x)
+            out = student_x
+
+        return out
+
+    def load_checkpoint(self, model, path):
+        checkpoint = torch.load(path)
+        model.load_state_dict(checkpoint['state_dict'])
+
+
+if __name__ == '__main__':
+    opt = parse_opts()
+    opt.pretrain_path = '../kinetics_pretrained_model/resnext-101-kinetics.pth'
+    teacher_model_path = 'Alexnet-final.pth'
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = teacher_student_net(opt, teacher_model_path, device)
+    print(model)
