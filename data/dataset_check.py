152d64a0e01c714eaf3896d154df3ef8a8756427
SM P <zzererer0471@gmail.com>
2019-05-29 02:30:15 +0900

    190529_1 commit - data/dataset_check.py
    다음 네가지 기능을 추가
    1. data_list/deepSBD.txt 분석
      > train, only_gradual, test 폴더 별 분포도
      > 중복 여부
    2. training dataset 분석
      > dataset의 cut, gradual, background의 디렉토리(train, gradual) 별 count
      > training에 사용된 video count
    3. ground truth 검증
      > cut, gradual의 갯수 및 비정상 데이터 검사
      > transition의 최댓, 최솟값 출력
    4. training dataset과 ground truth 비교
      > training dataset이 ground truth에 얼마나 포함되는지 체크

--- data/dataset_check.py
+++ data/dataset_check.py
@@ -8,137 +8,197 @@ if __name__ == '__main__':
     opt = parse_opts()
     root_dir = "ClipShots"
 
-    video_path_list = []
-    video_path_list.append(os.path.join(root_dir, "video_lists", "train.txt"))
-    video_path_list.append(os.path.join(root_dir, "video_lists", "only_gradual.txt"))
-    video_path_list.append(os.path.join(root_dir, "video_lists", "test.txt"))
-    video_name_list = []
-    for list_path in video_path_list:
+    video_list_root = os.path.join(root_dir, "video_lists")
+    video_location_list = ["train", "only_gradual", "test"]
+
+    video_path_dict = dict()
+    for video_location in video_location_list:
+        video_path_dict[video_location] = os.path.join(video_list_root, video_location + ".txt")
+
+    video_name_list = dict()
+    for video_location, list_path in video_path_dict.items():
         with open(list_path, 'r') as f:
-            video_name_list.append([line.strip('\n') for line in f.readlines()])
+            video_name_list[video_location] = dict()
+            video_name_list[video_location]["list"] = [line.strip('\n') for line in f.readlines()]
+            video_name_list[video_location]["count"] = len(video_name_list[video_location]["list"])
 
-    print("video list result : train({}), only_gradual({}), test({})".format(len(video_name_list[0]),len(video_name_list[1]),len(video_name_list[2])))
+    print("all video count : train({}), only_gradual({}), test({})".
+          format(video_name_list["train"]["count"],
+                 video_name_list["only_gradual"]["count"],
+                 video_name_list["test"]["count"]))
 
-    list_root_path = list()
     video_list_path = "data_list/deepSBD.txt"
-    count = {}
-    count["train"] = 0
-    count["only_gradual"] = 0
-    count["test"] = 0
-    count["duplicate"] = []
-    with open(video_list_path, 'r') as f:
-        for line in f.readlines():
-            check_duplicate = 0
-            words = line.split(' ')
-            if words[0] in video_name_list[0]:
-                count["train"] += 1
-                check_duplicate += 1
-            if words[0] in video_name_list[1]:
-                count["only_gradual"] += 1
-                check_duplicate += 1
-            if words[0] in video_name_list[2]:
-                count["test"] +=1
-                check_duplicate += 1
-            if check_duplicate > 1:
-                count["duplicate"] += [{"video_name": words[0], "duplicate": check_duplicate}]
 
-    print("video count : {}".format(count))
+    check_data_list = False
+    check_train_dataset = False
+    check_test_dataset = False
 
-    list_root_path.append(os.path.join(root_dir, 'videos', opt.train_subdir))
-    list_root_path.append(os.path.join(root_dir, 'videos', opt.only_gradual_subdir))
-    print(list_root_path, flush=True)
-    print("[INFO] reading : ", video_list_path, flush=True)
-    training_data = train_dataset(list_root_path, video_list_path, opt)
-    train_dataset = training_data.video_list
+    if check_data_list:
+        train_dataset_count = dict()
+        for video_location in video_location_list:
+            train_dataset_count[video_location] = 0
+            train_dataset_count["duplicate"] = list()
+        with open(video_list_path, 'r') as f:
+            for line in f.readlines():
+                check_duplicate = 0
+                words = line.split(' ')
+                for i, video_location in enumerate(video_location_list):
+                    if words[0] in video_name_list[video_location]["list"]:
+                        train_dataset_count[video_location] += 1
+                        check_duplicate += 1
+                if check_duplicate > 1:
+                    train_dataset_count["duplicate"] += [{"video_name": words[0], "duplicate": check_duplicate}]
 
-    total = {}
-    total["train"] = {}
-    total["only_gradual"] = {}
-    for key in total.keys():
-        total[key]["cut"] = 0
-        total[key]["gradual"] = 0
+        print("training dataset count : {}".format(train_dataset_count))
 
-    video = {}
-    video["train"] = []
-    video["only_gradual"] = []
-    for line in train_dataset:
-        line_video_path = line["video_path"]
-        if "train" in line_video_path:
-            video_location = "train"
-        else:
-            video_location = "only_gradual"
+    video_root = os.path.join(root_dir, "videos")
+    if check_train_dataset:
+        train_root_dir_list = list()
+        train_root_dir_list.append(os.path.join(video_root, opt.train_subdir))
+        train_root_dir_list.append(os.path.join(video_root, opt.only_gradual_subdir))
+        print("[INFO] reading : ", video_list_path, flush=True)
+        train_dataset = train_dataset(train_root_dir_list, video_list_path, opt)
+        train_data_list = train_dataset.video_list
 
-        if not line_video_path in video[video_location]:
-            video[video_location].append(line_video_path)
+        total_count = dict()
+        total_count["train"] = dict()
+        total_count["only_gradual"] = dict()
+        for key in total_count.keys():
+            total_count[key]["cut"] = 0
+            total_count[key]["gradual"] = 0
+            total_count[key]["background"] = 0
 
-        if line["label"] == 2:
-            total[video_location]["cut"] += 1
-        else:
-            total[video_location]["gradual"] += 1
-    print(total, "video number : train({}), only_gradual({})".format(len(video["train"]), len(video["only_gradual"])))
+        video_count = dict()
+        video_count["train"] = dict()
+        video_count["only_gradual"] = dict()
+        for key in video_count.keys():
+            video_count[key]["list"] = list()
+            video_count[key]["count"] = 0
 
-    root_dir = os.path.join("ClipShots", "videos", opt.test_subdir)
-    test_list_path = "ClipShots/video_lists/test.txt"
-    with open(test_list_path, 'r') as f:
-        video_name_list = [line.strip('\n') for line in f.readlines()]
-    test_data = test_dataset(root_dir, video_name_list)
-    video_list = test_data.video_list
+        for line in train_data_list:
+            line_video_path = line["video_path"]
+            if "train" in line_video_path:
+                video_location = "train"
+            else:
+                video_location = "only_gradual"
 
-    total = {}
-    total["train"] = {}
-    total["only_gradual"] = {}
-    total["test"] = {}
-    for key in total.keys():
-        total[key]["cut"] = 0
-        total[key]["gradual"] = 0
-        total[key]["total"] = 0
-        total[key]["else"] = {"length":0, "list":[]}
+            if not (line_video_path in video_count[video_location]["list"]):
+                video_count[video_location]["list"].append(line_video_path)
+                video_count[video_location]["count"] += 1
 
-    video = []
-    for line in video_list:
-        line_video_path = line["video_path"]
+            if line["label"] == 2:
+                total_count[video_location]["cut"] += 1
+            elif line["label"] == 1:
+                total_count[video_location]["gradual"] += 1
+            else:
+                total_count[video_location]["background"] += 1
 
-        if not line_video_path in video:
-            video.append(line_video_path)
+        print("train dataset total : {}".format(total_count))
+        print("train video count : train({}), only_gradual({})".
+              format(video_count["train"]["count"], video_count["only_gradual"]["count"]))
 
-    gts = {}
-    gt_base_dir = "ClipShots/annotations"
-    train_gt_dir = os.path.join(gt_base_dir, "train.json")
-    only_gradual_gt_dir = os.path.join(gt_base_dir, "only_gradual.json")
-    test_gt_dir = os.path.join(gt_base_dir, "test.json")
-    gts["train"] = json.load(open(train_gt_dir, 'r'))
-    gts["only_gradual"] = json.load(open(only_gradual_gt_dir, 'r'))
-    gts["test"] = json.load(open(test_gt_dir, 'r'))
+    if check_test_dataset:
+        test_root_dir = os.path.join(video_root, opt.test_subdir)
+        test_dataset = test_dataset(test_root_dir, video_name_list["test"]["list"])
+        test_data_list = test_dataset.video_list
 
-    max_transition = {"info": (0, 0), "value": 0}
-    min_transition = {"info": (0, 0), "value": 128}
-    for dir_name, gt in gts.items():
-        for video_name, data in gt.items():
-            _gts = data['transitions']
-            gt_cuts = [(begin, end) for begin, end in _gts if end - begin == 1]
-            gt_graduals = [(begin, end) for begin, end in _gts if end - begin > 1]
-            gt_else = [(begin, end) for begin, end in _gts if end - begin < 1]
-            total[dir_name]["cut"] += len(gt_cuts)
-            total[dir_name]["gradual"] += len(gt_graduals)
-            total[dir_name]["total"] += len(_gts)
-            total[dir_name]["else"]["list"] += gt_else
+        test_total = dict()
+        test_total["train"] = dict()
+        test_total["only_gradual"] = dict()
+        test_total["test"] = dict()
+        for key in test_total.keys():
+            test_total[key]["cut"] = 0
+            test_total[key]["gradual"] = 0
+            test_total[key]["total"] = 0
+            test_total[key]["else"] = {"length": 0, "list": list()}
 
-            for begin, end in gt_graduals:
-                if end - begin > max_transition["value"]:
-                    max_transition["value"] = end - begin
-                    max_transition["info"] = (begin, end)
-                if end - begin < min_transition["value"]:
-                    min_transition["value"] = end - begin
-                    min_transition["info"] = (begin, end)
+        test_video_count = []
+        for line in test_data_list:
+            line_video_path = line["video_path"]
 
-            # for begin, end in gt_else:
-            #     if begin - end > max_transition["value"]:
-            #         max_transition["value"] = begin - end
-            #         max_transition["info"] = (begin, end)
-            #     if begin - end < min_transition["value"]:
-            #         min_transition["value"] = begin - end
-            #         min_transition["info"] = (begin, end)
+            if not (line_video_path in test_video_count):
+                test_video_count.append(line_video_path)
 
-        total[dir_name]["else"]["length"] = len(total[dir_name]["else"]["list"])
-        print(total[dir_name])
-    print("transition length : max({}), min({})".format(max_transition, min_transition))
+        gts = {}
+        gt_base_dir = "ClipShots/annotations"
+        train_gt_dir = os.path.join(gt_base_dir, "train.json")
+        only_gradual_gt_dir = os.path.join(gt_base_dir, "only_gradual.json")
+        test_gt_dir = os.path.join(gt_base_dir, "test.json")
+        gts["train"] = json.load(open(train_gt_dir, 'r'))
+        gts["only_gradual"] = json.load(open(only_gradual_gt_dir, 'r'))
+        gts["test"] = json.load(open(test_gt_dir, 'r'))
+
+        max_transition = {"info": (0, 0), "value": 0}
+        min_transition = {"info": (0, 0), "value": 128}
+        for dir_name, gt in gts.items():
+            for video_name, data in gt.items():
+                _gts = data['transitions']
+                gt_cuts = [(begin, end) for begin, end in _gts if end - begin == 1]
+                gt_graduals = [(begin, end) for begin, end in _gts if end - begin > 1]
+                gt_else = [(begin, end) for begin, end in _gts if end - begin < 1]
+                test_total[dir_name]["cut"] += len(gt_cuts)
+                test_total[dir_name]["gradual"] += len(gt_graduals)
+                test_total[dir_name]["total"] += len(_gts)
+                test_total[dir_name]["else"]["list"] += gt_else
+
+                for begin, end in gt_graduals:
+                    if end - begin > max_transition["value"]:
+                        max_transition["value"] = end - begin
+                        max_transition["info"] = (begin, end)
+                    if end - begin < min_transition["value"]:
+                        min_transition["value"] = end - begin
+                        min_transition["info"] = (begin, end)
+
+                # for begin, end in gt_else:
+                #     if begin - end > max_transition["value"]:
+                #         max_transition["value"] = begin - end
+                #         max_transition["info"] = (begin, end)
+                #     if begin - end < min_transition["value"]:
+                #         min_transition["value"] = begin - end
+                #         min_transition["info"] = (begin, end)
+
+            test_total[dir_name]["else"]["length"] = len(test_total[dir_name]["else"]["list"])
+            print("{} : {}".format(dir_name, test_total[dir_name]))
+        print("transition length : max({}), min({})".format(max_transition, min_transition))
+
+    check_data_set = True
+    if check_data_set:
+        gts = {}
+        gt_base_dir = os.path.join(root_dir, "annotations")
+        train_gt_dir = os.path.join(gt_base_dir, "train.json")
+        only_gradual_gt_dir = os.path.join(gt_base_dir, "only_gradual.json")
+        gts["train"] = json.load(open(train_gt_dir, 'r'))
+        gts["only_gradual"] = json.load(open(only_gradual_gt_dir, 'r'))
+
+        correct = dict()
+        correct["cut"] = 0
+        correct["gradual"] = 0
+        correct["incorrect"] = 0
+        case = list()
+        with open(video_list_path, 'r') as f:
+            for i, line in enumerate(f.readlines()):
+                video_name, frame, label = line.split(' ')
+                frame, label = int(frame), int(label)
+
+                if i % 50000 == 0:
+                    print("finish {}".format(i))
+                video_location = ""
+                for gt_location in gts.keys():
+                    if video_name in video_name_list[gt_location]["list"]:
+                        video_location = gt_location
+                        break
+                find = False
+                if label > 0:
+                    for begin, end in gts[video_location][video_name]["transitions"]:
+                        if frame <= begin <= (frame+16) or frame <= end <= (frame+16) or begin <= frame <= end:
+                            if label == 2:
+                                correct["cut"] += 1
+                            elif label == 1:
+                                correct["gradual"] += 1
+                            else:
+                                pass
+                            find = True
+                            break
+                    if not find:
+                        correct["incorrect"] += 1
+        print(correct)
