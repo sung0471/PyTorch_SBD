5cde73654851f26c75ce475efc4cb32e2c09837a
SM P <zzererer0471@gmail.com>
2019-06-14 14:46:07 +0900

    190614_1 commit - lib/multiloss.py
    - loss의 type을 선택할 수 있게 설정

--- lib/multiloss.py
+++ lib/multiloss.py
@@ -8,17 +8,19 @@ class multiloss(nn.Module):
     def __init__(self, loss_type='new'):
         assert loss_type in ['origin', 'new']
         super(multiloss, self).__init__()
-        self.criterion = nn.CrossEntropyLoss()
+
         self.loss_type = loss_type
+        if self.loss_type == 'origin':
+            self.KLDiv = nn.KLDivLoss()
+        self.CrossEnt = nn.CrossEntropyLoss()
 
     def forward(self, predictions, targets):
         teacher_pred, student_pred = predictions
 
         if self.loss_type == 'origin':
             alpha = 0.5
-            KD_loss = nn.KLDivLoss()(F.log_softmax(student_pred, dim=1), F.softmax(teacher_pred, dim=1)) * alpha + \
-                      F.cross_entropy(student_pred, targets) * (1. - alpha)
-            loss = KD_loss
+            loss_teacher = self.KLDiv(F.log_softmax(student_pred, dim=1), F.softmax(teacher_pred, dim=1)) * alpha
+            loss_student = self.CrossEnt(student_pred, targets) * (1. - alpha)
         elif self.loss_type == 'new':
             # print(teacher_pred, student_pred, targets)
             # loss_teacher = self.criterion(student_pred, teacher_pred)
@@ -27,13 +29,12 @@ class multiloss(nn.Module):
             pred = pred.t()
             pred = pred.squeeze(0).to(torch.long)
             # print(pred)
-            loss_teacher = self.criterion(student_pred, pred)
-            loss_student = self.criterion(student_pred, targets)
+            loss_teacher = self.CrossEnt(student_pred, pred)
+            loss_student = self.CrossEnt(student_pred, targets)
             # loss_teacher /= 2
             # loss_student /= 2
-
-            loss = loss_teacher + loss_student
         else:
             assert False
 
+        loss = loss_teacher + loss_student
         return loss
