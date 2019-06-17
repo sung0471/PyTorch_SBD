59fe20f07bf8b1706174e413d349e31571956cdf
SM P <zzererer0471@gmail.com>
2019-06-17 13:32:52 +0900

    190617_1 commit - lib/multiloss.py
    - loss에 dual 모드 추가
      > student와 teacher 사이의 KL * 0.5 + student와 teacher GT 사이의 CE +
        student와 GT 사이의 CE * 0.5

--- lib/multiloss.py
+++ lib/multiloss.py
@@ -6,11 +6,11 @@ from torch.autograd import Variable
 
 class multiloss(nn.Module):
     def __init__(self, loss_type='new'):
-        assert loss_type in ['origin', 'new']
+        assert loss_type in ['origin', 'new', 'dual']
         super(multiloss, self).__init__()
 
         self.loss_type = loss_type
-        if self.loss_type == 'origin':
+        if self.loss_type in ['origin', 'dual']:
             self.KLDiv = nn.KLDivLoss()
         self.CrossEnt = nn.CrossEntropyLoss()
 
@@ -33,6 +33,14 @@ class multiloss(nn.Module):
             loss_student = self.CrossEnt(student_pred, targets)
             # loss_teacher /= 2
             # loss_student /= 2
+        elif self.loss_type == 'dual':
+            alpha = 0.5
+            _, pred = teacher_pred.topk(1, 1, True)
+            pred = pred.t()
+            pred = pred.squeeze(0).to(torch.long)
+            loss_teacher = self.KLDiv(F.log_softmax(student_pred, dim=1), F.softmax(teacher_pred, dim=1)) * alpha + \
+                           self.CrossEnt(student_pred, pred)
+            loss_student = self.CrossEnt(student_pred, targets) * (1. - alpha)
         else:
             assert False
 
