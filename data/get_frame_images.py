8a7e715876fd3aea8d2146ef312fb98f2ceee537
SM P <zzererer0471@gmail.com>
2019-06-18 15:58:40 +0900

    190618_1 commit - data/get_frame_images.py
    - 모든 test 데이터에 대해 이미지 출력하게끔 변경
    - 매 비디오마다 이미지 뽑는 처리시간과 전체 처리시간 출력
    - 중간에 hUoDOxOxK1I.mp4는 GT에 없는 값 추가

--- data/get_frame_images.py
+++ data/get_frame_images.py
@@ -1,9 +1,15 @@
 import os
 import json
 import cv2
+import time
+import datetime
 from PIL import Image
 
-file_name = "hUoDOxOxK1I.mp4"
+test_list_path = "ClipShots/video_lists/test.txt"
+with open(test_list_path, 'r') as f:
+    video_name_list = [line.strip('\n') for line in f.readlines()]
+
+# file_name = "hUoDOxOxK1I.mp4"
 
 gts = {}
 gt_base_dir = "ClipShots/annotations/"
@@ -13,33 +19,48 @@ test_gt_dir = os.path.join(gt_base_dir, "test.json")
 gts["train"] = json.load(open(train_gt_dir, 'r'))
 gts["only_gradual"] = json.load(open(only_gradual_gt_dir, 'r'))
 gts["test"] = json.load(open(test_gt_dir, 'r'))
-pos_info = gts["test"][file_name]["transitions"]
 
-video_base_dir = "ClipShots/videos/"
-video_path = os.path.join(video_base_dir, 'test', file_name)
-video_cap = cv2.VideoCapture(video_path)
+total_start_time = time.time()
+for i, file_name in enumerate(video_name_list):
+    per_video_start_time = time.time()
 
-images_path = os.path.join('images', file_name)
-if not os.path.exists('images'):
-    os.mkdir('images')
-if not os.path.exists(images_path):
-    os.mkdir(images_path)
-    os.mkdir(os.path.join(images_path, 'cut'))
-    os.mkdir(os.path.join(images_path, 'gradual'))
+    pos_info = gts["test"][file_name]["transitions"]
 
-for frame_pos, frame_end in pos_info:
-    if frame_end - frame_pos == 1:
-        transition_type = 'cut'
-        frame_pos -= 3
-        frame_end += 3
-    else:
-        transition_type = 'gradual'
-    video_cap.set(1, frame_pos)
-    for i in range(frame_pos, frame_end+1):
-        status, frame = video_cap.read()
-        if not status:
-            break
+    video_base_dir = "ClipShots/videos/"
+    video_path = os.path.join(video_base_dir, 'test', file_name)
+    video_cap = cv2.VideoCapture(video_path)
+
+    images_path = os.path.join('images', file_name)
+    if not os.path.exists('images'):
+        os.mkdir('images')
+    if not os.path.exists(images_path):
+        os.mkdir(images_path)
+        os.mkdir(os.path.join(images_path, 'cut'))
+        os.mkdir(os.path.join(images_path, 'gradual'))
+
+    if file_name == 'hUoDOxOxK1I.mp4':
+        pos_info += [[3113, 3133]]
+
+    for frame_pos, frame_end in pos_info:
+        if frame_end - frame_pos == 1:
+            transition_type = 'cut'
+            frame_pos -= 3
+            frame_end += 3
         else:
-            frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).convert('RGB')
-            image_path = os.path.join(images_path, transition_type)
-            frame.save(os.path.join(image_path, str(i) + '.png'))
+            transition_type = 'gradual'
+        video_cap.set(1, frame_pos)
+        for i in range(frame_pos, frame_end+1):
+            status, frame = video_cap.read()
+            if not status:
+                break
+            else:
+                frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).convert('RGB')
+                image_path = os.path.join(images_path, transition_type)
+                frame.save(os.path.join(image_path, str(i) + '.png'))
+
+    per_video_end_time = time.time() - per_video_start_time
+
+    print('video #{} {} : {}'.format(i, file_name, per_video_end_time), flush=True)
+
+total_time = time.time() - total_start_time
+print('Total video : {}'.format(datetime.timedelta(seconds=total_time)), flush=True)
