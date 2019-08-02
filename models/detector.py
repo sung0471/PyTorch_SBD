f04e171dcfacc1450716e0ca30a19f95697f89c8
SM P <zzererer0471@gmail.com>
2019-08-02 18:58:21 +0900

    190802_1 commit - models/detector.py
    - resnet 전체 코드 추가
      > stride = (2,2,2) 인것을 (1,2,2)로 수정
      > last_duration = sample_duration으로 수정
    
    - get_detector() 함수 추가
      > model_type=['resnet', 'resnext']와 depth를 파라미터로 받음
      > model_type과 depth에 따른 detector baseline return하는 함수

--- models/detector.py
+++ models/detector.py
@@ -6,7 +6,8 @@ import math
 from functools import partial
 import os
 
-__all__ = ['ResNeXt', 'resnext50', 'resnext101']
+__all__ = ['ResNet', 'resnet10', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152', 'resnet200',
+           'ResNeXt', 'resnext50', 'resnext101']
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -52,6 +53,185 @@ class DepthwiseSeparableConv3d(nn.Module):
         return x
 
 
+# Resnet code start
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm3d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3x3(planes, planes)
+        self.bn2 = nn.BatchNorm3d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv3d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm3d(planes)
+        self.conv2 = nn.Conv3d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm3d(planes)
+        self.conv3 = nn.Conv3d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm3d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, sample_size, sample_duration,
+                 shortcut_type='B', num_classes=400, use_depthwise=False):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv3d(3, 64, kernel_size=7, stride=(1, 2, 2),
+                               padding=(3, 3, 3), bias=False)
+        self.bn1 = nn.BatchNorm3d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], shortcut_type)
+        self.layer2 = self._make_layer(block, 128, layers[1], shortcut_type, stride=(1, 2, 2))
+        self.layer3 = self._make_layer(block, 256, layers[2], shortcut_type, stride=(1, 2, 2))
+        self.layer4 = self._make_layer(block, 512, layers[3], shortcut_type, stride=(1, 2, 2))
+        # last_duration = math.ceil(sample_duration / 16)
+        last_duration = sample_duration
+        last_size = math.ceil(sample_size / 32)
+        self.avgpool = nn.AvgPool3d((last_duration, last_size, last_size), stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm3d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+                # m.eval()
+
+    def _make_layer(self, block, planes, blocks, shortcut_type, stride=(1, 1, 1)):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            if shortcut_type == 'A':
+                downsample = partial(downsample_basic_block,
+                                     planes=planes * block.expansion,
+                                     stride=stride)
+            else:
+                bn = nn.BatchNorm3d(planes * block.expansion)
+                # bn.eval()
+                downsample = nn.Sequential(
+                    nn.Conv3d(self.inplanes, planes * block.expansion,
+                              kernel_size=1, stride=stride, bias=False),
+                    bn
+                )
+
+        layers = list()
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def multibox(self, resnet, shortcut_type='B', num_classes=3):
+        loc_layers = []
+        conf_layers = []
+        number_channel = [64, 128, 256, 512, 512]
+        pooling_size = [32, 16, 8, 4, 1]
+        box_number = [16, 8, 4, 2, 1]
+        box_length = [2, 4, 8, 16, 32]
+        box_center = []
+        for i in range(5):
+            self.inplanes = number_channel[i]
+            loc_layers += self._make_layer(BasicBlock, 2, 1, shortcut_type, stride=1)
+            loc_layers += nn.AvgPool3d((1, pooling_size[i], pooling_size[i]), stride=1)
+            conf_layers += self._make_layer(BasicBlock, num_classes, 1, shortcut_type, stride=1)
+            conf_layers += nn.AvgPool3d((1, pooling_size[i], pooling_size[i]), stride=1)
+
+    def forward(self, x):
+        # x = x.cuda()
+        # x = x.to(device)
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+    def load_weights(self, base_file):
+        other, ext = os.path.splitext(base_file)
+        if ext == '.pkl' or '.pth':
+            print('Loading weights into state dict...')
+            pretrained = torch.load(base_file, map_location=lambda storage, loc: storage)['state_dict']
+            pretrained = {"{}".format(s[7:]):v for s,v in pretrained.items()}
+            current_param = self.state_dict()
+            pretrained = {k: v for k, v in pretrained.items() if k in current_param and k[:2] != 'fc'}
+            current_param.update(pretrained)
+            print(pretrained.keys())
+            # print(self.state_dict().keys())
+            self.load_state_dict(current_param)
+            print('Finished!')
+        else:
+            print('Sorry only .pth and .pkl files supported.')
+
+
+# ResNeXt code start
 class ResNeXtBottleneck(nn.Module):
     expansion = 2
 
@@ -112,6 +292,7 @@ class ResNeXt(nn.Module):
         self.layer3 = self._make_layer(block, 512, layers[2], shortcut_type, cardinality, stride=(2, 2, 2))
         self.layer4 = self._make_layer(block, 1024, layers[3], shortcut_type, cardinality, stride=(2, 2, 2))
         last_duration = int(math.ceil(sample_duration / 16))
+        # last_duration = sample_duration
         last_size = int(math.ceil(sample_size / 32))
         self.avgpool = nn.AvgPool3d((last_duration, last_size, last_size), stride=1)  # 1
         self.fc = nn.Linear(cardinality * 32 * block.expansion, num_classes)
@@ -204,6 +385,83 @@ def get_fine_tuning_parameters(model, ft_begin_index):
             parameters.append({'params': v, 'lr': 0.0})
 
     return parameters
+
+
+def get_detector(model_type, depth, **kwargs):
+    assert model_type in ['resnet', 'resnext']
+    if model_type == 'resnet':
+        assert depth in [10, 18, 34, 50, 101, 152, 200]
+        if depth == 10:
+            return resnet34(**kwargs)
+        elif depth == 18:
+            return resnet18(**kwargs)
+        elif depth == 34:
+            return resnet34(**kwargs)
+        elif depth == 50:
+            return resnet50(**kwargs)
+        elif depth == 101:
+            return resnet101(**kwargs)
+        elif depth == 152:
+            return resnet152(**kwargs)
+        elif depth == 200:
+            return resnet200(**kwargs)
+    else:
+        assert depth in [50, 101, 152]
+        if depth == 50:
+            return resnext50(**kwargs)
+        elif depth == 101:
+            return resnext101(**kwargs)
+        elif depth == 152:
+            return resnext152(**kwargs)
+
+
+def resnet10(**kwargs):
+    """Constructs a ResNet-18 model.
+    """
+    model = ResNet(BasicBlock, [1, 1, 1, 1], **kwargs)
+    return model
+
+
+def resnet18(**kwargs):
+    """Constructs a ResNet-18 model.
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    return model
+
+
+def resnet34(**kwargs):
+    """Constructs a ResNet-34 model.
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def resnet50(**kwargs):
+    """Constructs a ResNet-50 model.
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def resnet101(**kwargs):
+    """Constructs a ResNet-101 model.
+    """
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    return model
+
+
+def resnet152(**kwargs):
+    """Constructs a ResNet-101 model.
+    """
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    return model
+
+
+def resnet200(**kwargs):
+    """Constructs a ResNet-101 model.
+    """
+    model = ResNet(Bottleneck, [3, 24, 36, 3], **kwargs)
+    return model
 
 
 def resnext50(**kwargs):
