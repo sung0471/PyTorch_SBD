369b2ce3eb28d9827050a881c5d8a0a910fda640
SM P <zzererer0471@gmail.com>
2019-08-12 14:45:09 +0900

    190812 commit-1 - data/dataset_for_detector.py
    - deepSBD.txt로 dataset 제작하는 코드 추가
      > deepSBD의 데이터를 한 줄 씩 불러옴.
      > 한 줄 마다 gt를 매칭한 뒤
      > 해당하는 gt 범위를 새로운 파일에 저장
    
    - if make_dataset_from_groundTruth:
      > gt를 불러와서 저장하는 코드를 전역으로 전환

--- data/dataset_for_detector.py
+++ data/dataset_for_detector.py
@@ -1,37 +1,38 @@
 import os
 import json
 
-make_dataset_from_groundTruth = True
-merge_dataset_with_deepSBD = True
-check_data_list = True
+make_dataset_from_groundTruth = False
+merge_dataset_with_deepSBD = False
+make_dataset_using_deepSBD = True
+check_count_of_data_list = True
 
 data_list_root = 'data_list'
-new_file_name = 'detector_list.txt'
+new_file_name = 'detector.txt'
 dataset_path = os.path.join(data_list_root, new_file_name)
 
+dataset_root = 'ClipShots'
+video_list_dir = os.path.join(dataset_root, 'video_lists')
+video_gt_dir = os.path.join(dataset_root, 'annotations')
+category = ['train', 'only_gradual']
+
+gt = dict()
+for dir_name in category:
+    video_list_path = os.path.join(video_list_dir, dir_name + '.txt')
+    video_gt_path = os.path.join(video_gt_dir, dir_name + '.json')
+
+    gt[dir_name] = dict()
+    video_gt = json.load(open(video_gt_path, 'r'))
+    for video_name, data in video_gt.items():
+        gt[dir_name][video_name] = dict()
+        gt[dir_name][video_name]['cut'] = list()
+        gt[dir_name][video_name]['gradual'] = list()
+        for start, end in data['transitions']:
+            if end - start == 1:
+                gt[dir_name][video_name]['cut'] += [(start, end)]
+            elif end - start > 1:
+                gt[dir_name][video_name]['gradual'] += [(start, end)]
+
 if make_dataset_from_groundTruth:
-    video_root = 'ClipShots'
-    video_list_dir = os.path.join('ClipShots', 'video_lists')
-    video_gt_dir = os.path.join('ClipShots', 'annotations')
-    category = ['train', 'only_gradual']
-
-    gt = dict()
-    for dir_name in category:
-        video_list_path = os.path.join(video_list_dir, dir_name + '.txt')
-        video_gt_path = os.path.join(video_gt_dir, dir_name + '.json')
-
-        gt[dir_name] = dict()
-        video_gt = json.load(open(video_gt_path, 'r'))
-        for video_name, data in video_gt.items():
-            gt[dir_name][video_name] = dict()
-            gt[dir_name][video_name]['cut'] = list()
-            gt[dir_name][video_name]['gradual'] = list()
-            for start, end in data['transitions']:
-                if end-start == 1:
-                    gt[dir_name][video_name]['cut'] += [(start, end)]
-                elif end-start > 1:
-                    gt[dir_name][video_name]['gradual'] += [(start, end)]
-
     count = dict()
     with open(dataset_path, 'wt', encoding='utf-8') as f:
         for dir_name, data_list in gt.items():
@@ -73,7 +74,59 @@ if merge_dataset_with_deepSBD:
             if class_type == 0:
                 destination.write('{} {} {}\n'.format(video_name, start, class_type))
 
-if check_data_list:
+if make_dataset_using_deepSBD:
+    deepSBD_path = os.path.join(data_list_root, 'deepSBD.txt')
+    destination = open(dataset_path, 'wt', encoding='utf-8')
+    transition_type = ['no', 'gradual', 'cut']
+    sample_duration = 16
+    count = [0, 0]
+    with open(deepSBD_path, 'rt', encoding='utf-8') as f:
+        for line in f.readlines():
+            video_name, start, label = line.split(' ')
+            start = int(start)
+            label = int(label.split('\n')[0])
+            for i in range(2):
+                video_dir = os.path.join(dataset_root, 'videos/', category[i])
+                video_path = os.path.join(video_dir, video_name)
+                if os.path.exists(video_path):
+                    dir_name = category[i]
+                    break
+                elif i == 1:
+                    assert (os.path.exists(video_path))
+                else:
+                    continue
+
+            if label != 0:
+                end = start + sample_duration - 1
+                check = False
+                for gt_s, gt_e in gt[dir_name][video_name][transition_type[label]]:
+                    if start <= gt_s < end <= gt_e:
+                        check = not check
+                    elif start <= gt_s < gt_e <= end:
+                        check = not check
+                    elif gt_s <= start < end <= gt_e:
+                        check = not check
+                    elif gt_s <= start < gt_e <= end:
+                        check = not check
+                    else:
+                        continue
+
+                    if check:
+                        destination.write('{} {} {} {} {}\n'.format(video_name, start, label, gt_s, gt_e))
+                        break
+                count[check] += 1
+                if check:
+                    check = not check
+                    print('{} {} {} {} {}'.format(video_name, start, label, gt_s, gt_e))
+                else:
+                    print('{}/{} : no gt'.format(dir_name, video_name))
+            else:
+                destination.write('{} {} {}\n'.format(video_name, start, label))
+                count[1] += 1
+
+    print('gt/no_gt : {}/{}'.format(count[1], count[0]))
+
+if check_count_of_data_list:
     count = dict()
     with open(dataset_path, 'rt', encoding='utf-8') as f:
         for line in f.readlines():
