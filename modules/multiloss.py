7609f9ced74866d9700bcba8a9a362a48e642938
SM P <zzererer0471@gmail.com>
2019-08-28 23:18:29 +0900

    190828 commit-1 - modules/multiloss.py
    - Multiloss의 param으로 extra_layers, sample_duration을 받음
    - default_bar 설정하는 코드 추가
      > sample_duration=16, 32인 케이스 추가
      > default_bar의 center 좌표, length default 설정
    - loc_target, conf_target > .clone().detach()로 설정
      > loc_target : encoding 함수 적용
    - extra_layer를 사용하는 부분과 미사용 부분 구분하여 forward 진행
      > extra_layer를 사용하는 부분은 미완성

--- modules/multiloss.py
+++ modules/multiloss.py
@@ -1,25 +1,60 @@
 import torch
 import torch.nn as nn
 from modules.layers.multi_detector import MultiDetector
+from lib.utils import encoding
 
 
 class MultiLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, extra_layers=False, sample_duration=16):
         super(MultiLoss, self).__init__()
 
+        self.extra_layers = extra_layers
+        self.sample_duration = sample_duration
         self.reg_loss = nn.SmoothL1Loss()
         # self.reg_loss = nn.MSELoss()
         self.conf_loss = nn.CrossEntropyLoss()
 
+        self.default_bar_number_list = dict()
+        self.default_bar_number_list[16] = [15, 7, 3, 1]
+        self.default_bar_number_list[32] = [31, 15, 7, 3, 1]
+        self.default_bar = dict()
+        self.default_bar[16] = torch.zeros(26, 2)
+        self.default_bar[32] = torch.zeros(57, 2)
+        for sample_duration in self.default_bar_number_list.keys():
+            count = 0
+            length = 2
+            for default_bar_number in self.default_bar_number_list[sample_duration]:
+                for start in range(default_bar_number):
+                    self.default_bar[sample_duration][count][0] = start * (length / 2)
+                    self.default_bar[sample_duration][count][1] = start * (length / 2) + length - 1
+                    count += 1
+                length *= 2
+
     def forward(self, predictions, targets):
         loc_pred, conf_pred = predictions
-        loc_target, conf_target = targets[:, :2], targets[:, -1].to(torch.long)
+        if not self.extra_layers:
+            loc_target = targets[:, :-1].clone().detach().data
+            conf_target = targets[:, -1].clone().detach().to(torch.long).data
+            loc_target = encoding(loc_target)
 
-        alpha = 0.5
-        loss_loc = self.reg_loss(loc_pred, loc_target) * alpha
-        loss_conf = self.conf_loss(conf_pred, conf_target) * (1. - alpha)
+            alpha = 0.5
+            loss_loc = self.reg_loss(loc_pred, loc_target) * alpha
+            loss_conf = self.conf_loss(conf_pred, conf_target) * (1. - alpha)
+        else:
+            batch_size = targets.size(0)
+            default_bar_num = self.default_bar[self.sample_duration].size(0)
+            loc_t = torch.Tensor(batch_size, default_bar_num, 2)
+            conf_t = torch.LongTensor(batch_size, default_bar_num)
+            for idx in range(batch_size):
+                truths = targets[idx, :, :-1].clone().detach().data
+                labels = targets[idx, :, -1].clone().detach().to(torch.long).data
+
+            # alpha = 0.5
+            # loss_loc = self.reg_loss(loc_pred, loc_target) * alpha
+            # loss_conf = self.conf_loss(conf_pred, conf_target) * (1. - alpha)
 
         loss = loss_loc + loss_conf
+
         return loss
 
 
@@ -32,6 +67,6 @@ if __name__ == '__main__':
     print('conf : {}, size : {}'.format(pred[1], pred[1].size()))
 
     targets = torch.ones([1, 3])
-    criterion = MultiLoss()
+    criterion = MultiLoss(extra_layers=True, sample_duration=16)
     loss = criterion(pred, targets)
     print(loss)
