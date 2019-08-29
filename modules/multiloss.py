26d745f96100a8ddd664b1e92bfbcda8353c93d0
SM P <zzererer0471@gmail.com>
2019-08-30 04:14:01 +0900

    190830 commit-1 - modules/multiloss.py
    - multiloss : multilayer 시 진행할 코드 일부 추가(완성X)

--- modules/multiloss.py
+++ modules/multiloss.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from modules.layers.multi_detector import MultiDetector
-from lib.utils import encoding
+from lib.utils import encoding, cal_iou
 
 
 class MultiLoss(nn.Module):
@@ -34,8 +34,8 @@ class MultiLoss(nn.Module):
         loc_pred, conf_pred = predictions
         if not self.extra_layers:
             loc_target = targets[:, :-1].clone().detach().data
+            loc_target = encoding(loc_target, self.sample_duration)
             conf_target = targets[:, -1].clone().detach().to(torch.long).data
-            loc_target = encoding(loc_target)
 
             alpha = 0.5
             loss_loc = self.reg_loss(loc_pred, loc_target) * alpha
@@ -48,6 +48,31 @@ class MultiLoss(nn.Module):
             for idx in range(batch_size):
                 truths = targets[idx, :, :-1].clone().detach().data
                 labels = targets[idx, :, -1].clone().detach().to(torch.long).data
+                # jaccard index
+                overlaps = cal_iou(
+                    truths,
+                    self.default_bar_num
+                )
+                # (Bipartite Matching)
+                # [1,num_objects] best prior for each ground truth
+                best_prior_overlap, best_prior_idx = overlaps.max(1, keepdim=True)
+                # [1,num_priors] best ground truth for each prior
+                best_truth_overlap, best_truth_idx = overlaps.max(0, keepdim=True)
+                best_truth_idx.squeeze_(0)
+                best_truth_overlap.squeeze_(0)
+                best_prior_idx.squeeze_(1)
+                best_prior_overlap.squeeze_(1)
+                best_truth_overlap.index_fill_(0, best_prior_idx, 2)  # ensure best prior
+                # TODO refactor: index  best_prior_idx with long tensor
+                # ensure every gt matches with its prior of max overlap
+                for j in range(best_prior_idx.size(0)):
+                    best_truth_idx[best_prior_idx[j]] = j
+                matches = truths[best_truth_idx]  # Shape: [num_priors,4]
+                conf = labels[best_truth_idx] + 1  # Shape: [num_priors]
+                conf[best_truth_overlap < 0.5] = 0  # label as background
+                loc = encoding(matches)
+                loc_t[idx] = loc  # [num_priors,4] encoded offsets to learn
+                conf_t[idx] = conf  # [num_priors] top class label for each prior
 
             # alpha = 0.5
             # loss_loc = self.reg_loss(loc_pred, loc_target) * alpha
