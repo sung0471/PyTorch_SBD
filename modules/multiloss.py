5ee470b2b35b695bc6c03150c254f3b4ea1383c8
SM P <zzererer0471@gmail.com>
2019-09-03 06:04:07 +0900

    190903 commit-1 - modules/multiloss.py
    - MultiLoss 선언 시, device, num_classes, neg_ratio를 param으로 받음
    - default_bar를 lib/utils.py에서 가져옴
    - forward
      > total_length = sample_duration - 1 추가
      > default_bar.to(device) 설정
      > reg_loss, conf_loss의 param(reduction='sum') 설정 : 자동으로 평균내지 않음
      > loc_t, conf_t : 각 default_bar와 가장 많이 겹치는 GT의 좌표(encoding) + label
      > reg_loss : background가 아닌 것만 학습
      > conf_loss
        - loss_conf : log_sum_exp - conf 계산
        - pos vs neg의 비율 = 1:3으로 학습
        - cross_entropy 계산
      > 계산한 reg_loss와 conf_loss를 pos_num 수로 나눔

--- modules/multiloss.py
+++ modules/multiloss.py
@@ -1,82 +1,128 @@
 import torch
 import torch.nn as nn
 from modules.layers.multi_detector import MultiDetector
-from lib.utils import encoding, cal_iou
+from lib.utils import encoding, cal_iou, log_sum_exp, default_bar
 
 
 class MultiLoss(nn.Module):
-    def __init__(self, extra_layers=False, sample_duration=16):
+    def __init__(self, device, extra_layers=False, sample_duration=16, num_classes=3, neg_ratio=3):
         super(MultiLoss, self).__init__()
 
+        self.device = device
         self.extra_layers = extra_layers
         self.sample_duration = sample_duration
+        self.num_classes = num_classes
+        self.negpos_ratio = neg_ratio
+
         self.reg_loss = nn.SmoothL1Loss()
         # self.reg_loss = nn.MSELoss()
         self.conf_loss = nn.CrossEntropyLoss()
-
-        self.default_bar_number_list = dict()
-        self.default_bar_number_list[16] = [15, 7, 3, 1]
-        self.default_bar_number_list[32] = [31, 15, 7, 3, 1]
-        self.default_bar = dict()
-        self.default_bar[16] = torch.zeros(26, 2)
-        self.default_bar[32] = torch.zeros(57, 2)
-        for sample_duration in self.default_bar_number_list.keys():
-            count = 0
-            length = 2
-            for default_bar_number in self.default_bar_number_list[sample_duration]:
-                for start in range(default_bar_number):
-                    self.default_bar[sample_duration][count][0] = start * (length / 2)
-                    self.default_bar[sample_duration][count][1] = start * (length / 2) + length - 1
-                    count += 1
-                length *= 2
+        self.default_bar = default_bar(sample_duration=sample_duration)
 
     def forward(self, predictions, targets):
+        total_length = self.sample_duration - 1
         loc_pred, conf_pred = predictions
         if not self.extra_layers:
+            # loc_pred : [batch_size, 2]
+            # conf_pred : [batch_size, 3]
             loc_target = targets[:, :-1].clone().detach().data
-            loc_target = encoding(loc_target, self.sample_duration)
+            loc_target = encoding(loc_target, total_length)
             conf_target = targets[:, -1].clone().detach().to(torch.long).data
 
             alpha = 0.5
             loss_loc = self.reg_loss(loc_pred, loc_target) * alpha
             loss_conf = self.conf_loss(conf_pred, conf_target) * (1. - alpha)
         else:
+            # loc_pred : [batch_size, default_bar_num, 2]
+            # conf_pred : [batch_size, default_bar_num, 3]
             batch_size = targets.size(0)
-            default_bar_num = self.default_bar[self.sample_duration].size(0)
+            self.default_bar = self.default_bar.to(self.device)
+            default_bar_num = self.default_bar.size(0)
+            self.reg_loss = nn.SmoothL1Loss(reduction='sum')
+            self.conf_loss = nn.CrossEntropyLoss(reduction='sum')
+
             loc_t = torch.Tensor(batch_size, default_bar_num, 2)
             conf_t = torch.LongTensor(batch_size, default_bar_num)
             for idx in range(batch_size):
-                truths = targets[idx, :, :-1].clone().detach().data
-                labels = targets[idx, :, -1].clone().detach().to(torch.long).data
+                truths = targets[idx, :-1].view(-1, 2).data                # [1, 2]
+                labels = targets[idx, -1].view(-1, 1).to(torch.long).data  # [1, 1]
+                default = self.default_bar.clone().detach().data           # [default_bar_num, 2]
                 # jaccard index
-                overlaps = cal_iou(
+                # truths = truths.view(-1, 1, 2)      # [1, 1, 2]
+                # labels = labels.view(-1, 1)         # [1, 1]
+                # default = default.view(1, -1, 2)    # [1, default_bar_num, 2]
+                overlaps = cal_iou(                 # [1, default_bar_num]
                     truths,
-                    self.default_bar_num
+                    default,
+                    default_bar_num
                 )
                 # (Bipartite Matching)
-                # [1,num_objects] best prior for each ground truth
-                best_prior_overlap, best_prior_idx = overlaps.max(1, keepdim=True)
-                # [1,num_priors] best ground truth for each prior
-                best_truth_overlap, best_truth_idx = overlaps.max(0, keepdim=True)
-                best_truth_idx.squeeze_(0)
-                best_truth_overlap.squeeze_(0)
-                best_prior_idx.squeeze_(1)
-                best_prior_overlap.squeeze_(1)
+                # [1(gt_num), 1] best prior for each ground truth
+                best_prior_overlap, best_prior_idx = overlaps.max(1, keepdim=True)  # GT와 가장 많이 겹치는 default
+                # [1, default_bar_num] best ground truth for each prior
+                best_truth_overlap, best_truth_idx = overlaps.max(0, keepdim=True)  # default와 가장 많이 겹치는 GT
+                best_prior_idx.squeeze_(1)          # [1(gt_num)]
+                best_prior_overlap.squeeze_(1)      # [1(gt_num)]
+                best_truth_idx.squeeze_(0)          # [default_bar_num]
+                best_truth_overlap.squeeze_(0)      # [default_bar_num]
                 best_truth_overlap.index_fill_(0, best_prior_idx, 2)  # ensure best prior
                 # TODO refactor: index  best_prior_idx with long tensor
                 # ensure every gt matches with its prior of max overlap
                 for j in range(best_prior_idx.size(0)):
                     best_truth_idx[best_prior_idx[j]] = j
-                matches = truths[best_truth_idx]  # Shape: [num_priors,4]
-                conf = labels[best_truth_idx] + 1  # Shape: [num_priors]
+                matches = truths[best_truth_idx]  # Shape: [default_bar_num,2]
+                conf = labels[best_truth_idx] + 1  # Shape: [default_bar_num]
                 conf[best_truth_overlap < 0.5] = 0  # label as background
-                loc = encoding(matches)
-                loc_t[idx] = loc  # [num_priors,4] encoded offsets to learn
-                conf_t[idx] = conf  # [num_priors] top class label for each prior
+                assert matches.size() == self.default_bar.size(),\
+                    "matches_size : {}, default_bar_size : {}".format(matches.size(), self.default_bar.size())
+                loc = encoding(matches, total_length, default_bar=self.default_bar)
+                loc_t[idx] = loc  # [default_bar_num,2] encoded offsets to learn
+                conf_t[idx] = conf.squeeze(1)  # [default_bar_num] top class label for each prior
 
             # alpha = 0.5
             # loss_loc = self.reg_loss(loc_pred, loc_target) * alpha
             # loss_conf = self.conf_loss(conf_pred, conf_target) * (1. - alpha)
+
+            # wrap targets
+            with torch.no_grad():
+                loc_t = loc_t.to(self.device)
+                conf_t = conf_t.to(self.device)
+
+            pos = conf_t > 0
+
+            # Localization Loss (Smooth L1)
+            # Shape: [batch, default_bar_num, 2]
+            pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_pred)
+            loc_p = loc_pred[pos_idx].view(-1, 2)
+            loc_t = loc_t[pos_idx].view(-1, 2)
+            loss_loc = self.reg_loss(loc_p, loc_t)
+
+            # Compute max conf across batch for hard negative mining
+            # batch_conf, loss_conf : [batch_size * default_bar_num, num_classes]
+            batch_conf = conf_pred.view(-1, self.num_classes)
+            loss_conf = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
+
+            # Hard Negative Mining
+            loss_conf = loss_conf.view(batch_size, -1)  # [batch_size, default_bar_num]
+            loss_conf[pos] = 0  # filter out pos boxes for now / background 아닌 conf를 filtering
+            _, loss_idx = loss_conf.sort(1, descending=True)    # conf가 큰 idx의 내림차순 : [8, 26]
+            _, idx_rank = loss_idx.sort(1)  # 각 idx의 등수 : [batch_size, default_bar_num]
+            num_pos = pos.long().sum(1, keepdim=True)
+            num_neg = torch.clamp(self.negpos_ratio * num_pos, max=pos.size(1) - 1)     # [batch_size, 1]
+            neg = idx_rank < num_neg.expand_as(idx_rank)         # [batch_size, default_bar_num]
+
+            # Confidence Loss Including Positive and Negative Examples
+            pos_idx = pos.unsqueeze(2).expand_as(conf_pred)
+            neg_idx = neg.unsqueeze(2).expand_as(conf_pred)
+            conf_p = conf_pred[(pos_idx + neg_idx).gt(0)].view(-1, self.num_classes)
+            targets_weighted = conf_t[(pos + neg).gt(0)]
+            loss_conf = self.conf_loss(conf_p, targets_weighted)
+
+            # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
+
+            N = num_pos.data.sum()
+            loss_loc /= N
+            loss_conf /= N
 
         loss = loss_loc + loss_conf
 
@@ -84,14 +130,21 @@ class MultiLoss(nn.Module):
 
 
 if __name__ == '__main__':
+    from models.detector import Bottleneck
+    block = Bottleneck
     kernel_size = (16, 4, 4)
-    layer = MultiDetector(512 * 4, kernel_size=kernel_size)
-    input_t = torch.ones([1, 512*4, 16, 4, 4], dtype=torch.float32)
-    pred = layer(input_t)
-    print('loc : {}, size : {}'.format(pred[0], pred[0].size()))
-    print('conf : {}, size : {}'.format(pred[1], pred[1].size()))
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    targets = torch.ones([1, 3])
-    criterion = MultiLoss(extra_layers=True, sample_duration=16)
-    loss = criterion(pred, targets)
-    print(loss)
+    layer = MultiDetector(block, 512, kernel_size=kernel_size, num_classes=3, extra_layers=True, phase='train')
+    input_t = torch.ones([8, 512*4, 16, 4, 4], dtype=torch.float32)
+    boundary = torch.zeros(8)
+    for i in range(8):
+        boundary[i] = i * 8
+    data = layer.to(device)(input_t.to(device), boundary.to(device))
+    print('loc_size : {}'.format(data[0].size()))
+    print('conf_size : {}'.format(data[1].size()))
+
+    targets = torch.ones([8, 3]).to(device)
+    criterion = MultiLoss(device, extra_layers=True, sample_duration=16)
+    loss = criterion(data, targets)
+    print(loss, loss.size())
