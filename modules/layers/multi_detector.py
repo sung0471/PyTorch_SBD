397d1bd41a95d9cf417d64ccdfb0a51ead941652
SM P <zzererer0471@gmail.com>
2019-09-04 05:10:36 +0900

    190904 commit-1 - modules/layers/multi_detector.py
    - conf_thresh, nms_thresh, top_k 추가
      > conf_thresh : test시 얻는 결과들의 conf 하한선(0.01)
      > nms_thresh : nms시 filtering하는 하한선(0.45)
      > top_k : nms시 class당 filtering하여 얻는 데이터 갯수
    - detection하는 부분 추가
      > conf >= 0.01인 결과 추출
      > nms를 진행하여 class당 5개 결과 추출(background 제외)
      > nms가 진행된 결과 중 conf >= 0.6만 추출
      > 추출된 결과에 boundaries add
      > return([bars_num, 2], [bars_num])

--- modules/layers/multi_detector.py
+++ modules/layers/multi_detector.py
@@ -1,16 +1,20 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from lib.utils import channel_list, decoding, default_bar
+from lib.utils import channel_list, decoding, default_bar, nms
 
 
 class MultiDetector(nn.Module):
-    def __init__(self, block, in_planes, kernel_size=(16, 2, 2), num_classes=3, extra_layers=False, phase='train'):
+    def __init__(self, block, in_planes, kernel_size=(16, 2, 2), num_classes=3, extra_layers=False,
+                 phase='train', conf_thresh=0.01, nms_thresh=0.45, top_k=5):
         super(MultiDetector, self).__init__()
 
         self.num_classes = num_classes
         self.extra_layers = extra_layers
         self.phase = phase
+        self.conf_thresh = conf_thresh
+        self.nms_thresh = nms_thresh
+        self.top_k = top_k
         self.sample_duration = kernel_size[0]
         self.default_bar = default_bar(sample_duration=self.sample_duration)
 
@@ -92,22 +96,77 @@ class MultiDetector(nn.Module):
             out = (loc_x.view(batch_size, -1, 2), conf_x.view(batch_size, -1, self.num_classes))
 
             # detection
-            # loc[8, 26, 2], conf[8, 26, 1]
+            # loc[8, 26, 2], conf[8, 26, 3]
             if self.phase == 'test':
                 loc, conf = out
                 frame_pos = torch.zeros(loc.size(0), loc.size(1), loc.size(2))
                 labels = torch.zeros(conf.size(0), conf.size(1), 1)
+
+                output = torch.zeros(batch_size, self.num_classes, self.top_k, 3)
+                conf_pred = conf.transpose(2, 1)
                 for i in range(batch_size):
                     total_length = self.sample_duration - 1
 
-                    assert loc.size(1) == self.default_bar.size(0)
-                    frame_pos[i, :] = decoding(loc[i, :], total_length, default_bar=self.default_bar)
-                    frame_pos[i, :] += start_boundaries[i]                  # [default_num, 2]
-                    label = conf[i, :]
-                    labels[i, :] = torch.argmax(label, dim=1).view(-1, 1)   # [default_num, 1]
+                    # assert loc.size(1) == self.default_bar.size(0)
+                    # frame_pos[i, :] = decoding(loc[i, :], total_length, default_bar=self.default_bar)
+                    # frame_pos[i, :] += start_boundaries[i]                  # [default_num, 2]
+                    # label = conf[i, :]
+                    # labels[i, :] = torch.argmax(label, dim=1).view(-1, 1)   # [default_num, 1]
 
-                # frame_pos = [batch_size, default_num, 2]
-                # lables = [batch_size, default_num, 1]
+                    decoded_bars = decoding(loc[i, :], total_length, default_bar=self.default_bar)  # [default_num, 2]
+                    conf_scores = conf_pred[i].clone().detach()   # [3, default_num]
+
+                    for cl in range(1, self.num_classes):
+                        c_mask = conf_scores[cl].gt(self.conf_thresh)   # [default_num]
+                        # for i in default_num,
+                        # if conf[i] > conf_thresh, num += 1
+                        scores = conf_scores[cl][c_mask]    # [num]
+                        if scores.size(0) == 0:
+                            continue
+                        l_mask = c_mask.unsqueeze(1).expand_as(decoded_bars)    # [default_num, 2]
+                        bars = decoded_bars[l_mask].view(-1, 2)     # [num, 2]
+                        # idx of highest scoring and non-overlapping boxes per class
+                        ids, count = nms(bars, scores, self.nms_thresh, self.top_k)
+                        output[i, cl, :count] = \
+                            torch.cat((bars[ids[:count]],
+                                       scores[ids[:count]].unsqueeze(1)), 1)    # [count, 3]
+
+                # [batch_size, all_result_bars_num, 3]
+                # all_result_bars_num : 클래스 별 bar의 갯수(최대 top_k * (num_classes - 1))
+                # 3 : start, end, conf
+                flt = output.contiguous().view(batch_size, -1, 3)
+                _, idx = flt[:, :, -1].sort(1, descending=True)     # [batch_size, bars_num]
+                _, rank = idx.sort(1)                               # [batch_size, bars_num], 각 idx 의 등수
+                flt[(rank < self.top_k).unsqueeze(-1).expand_as(flt)].fill_(0)
+
+                # [batch_size, class, num_bars, data]
+                # data = [start, end, conf]
+                pred_num = torch.zeros(batch_size, self.num_classes, dtype=torch.int32)
+                for batch_num in range(batch_size):
+                    for cls in range(self.num_classes):
+                        i = 0
+                        while output[batch_num, cls, i, -1] >= 0.6:
+                            output[batch_num, cls, i, :-1] = torch.round(
+                                output[batch_num, cls, i, :-1] + start_boundaries[batch_num])
+                            i += 1
+                            if i == self.top_k:
+                                break
+                        pred_num[batch_num, cls] = i
+
+                total_bars_num = pred_num.int().sum().clone().detach().item()
+                frame_pos = torch.zeros(total_bars_num, 2, dtype=torch.int32)
+                labels = torch.zeros(total_bars_num, dtype=torch.int32)
+                num = 0
+                for batch_num in range(batch_size):
+                    for cls in range(self.num_classes):
+                        result_num = pred_num[batch_num, cls].data
+                        for i in range(result_num):
+                            frame_pos[num] = output[batch_num, cls, i, :-1]
+                            labels[num] = cls
+                            num += 1
+
+                # frame_pos = [bars_num, 2]
+                # labels = [bars_num]
                 out = (frame_pos, labels)
 
         return out
