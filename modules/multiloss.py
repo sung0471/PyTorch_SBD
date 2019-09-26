47d97a0e6016428c892da823dee818e488a1e7ac
SM P <zzererer0471@gmail.com>
2019-09-26 21:18:28 +0900

    190926 - commit-1 modules/multiloss.py
    - Multiloss : param 에 negative_threshold 추가
      > negative_threshold : train_type 에 따라 다르게 추가됨
        - normal=(0.33, 0.5), cut=0.33, gradual=0.5
      > do_hard_negative_mining 조건부 제거
      > return : loss > loss_loc, loss_conf 으로 수정
        - main 에서 graph 출력하기 위함

--- modules/multiloss.py
+++ modules/multiloss.py
@@ -5,7 +5,8 @@ from lib.utils import encoding, cal_iou, log_sum_exp, default_bar
 
 
 class MultiLoss(nn.Module):
-    def __init__(self, device, extra_layers=False, sample_duration=16, num_classes=3, data_type='normal', neg_ratio=3):
+    def __init__(self, device, extra_layers=False, sample_duration=16, num_classes=3,
+                 data_type='normal', neg_ratio=3, neg_threshold=(0.33, 0.5)):
         super(MultiLoss, self).__init__()
 
         self.device = device
@@ -13,6 +14,7 @@ class MultiLoss(nn.Module):
         self.sample_duration = sample_duration
         self.num_classes = num_classes
         self.negpos_ratio = neg_ratio
+        self.neg_threshold = neg_threshold
 
         self.reg_loss = nn.SmoothL1Loss()
         # self.reg_loss = nn.MSELoss()
@@ -77,9 +79,11 @@ class MultiLoss(nn.Module):
                     best_truth_idx[best_prior_idx[j]] = j
                 matches = truths[best_truth_idx]    # Shape: [default_bar_num, 2]
                 conf = labels[best_truth_idx] + 1   # Shape: [default_bar_num]
+
                 background_conf_idx = conf == 1         # get index of background
-                conf[best_truth_overlap < 0.33] = 0     # label as negative
+                conf[best_truth_overlap < self.neg_threshold] = 0     # label as negative
                 conf[background_conf_idx] = 1           # set label to background
+
                 assert matches.size() == self.default_bar.size(),\
                     "matches_size : {}, default_bar_size : {}".format(matches.size(), default.size())
                 loc = encoding(matches, total_length, default_bar=default)
@@ -96,48 +100,41 @@ class MultiLoss(nn.Module):
             loc_t = loc_t[pos_idx].view(-1, 2)
             loss_loc = self.reg_loss(loc_p, loc_t)
 
-            do_hard_negative_mining = True
-            if do_hard_negative_mining:
-                # Compute max conf across batch for hard negative mining
-                # batch_conf : [batch_size * default_bar_num, num_classes]
-                # loss_conf : [batch_size * default_bar_num, 1]
-                batch_conf = conf_pred.view(-1, self.num_classes)
-                conf_t = torch.clamp(conf_t - 1, min=0)
-                loss_conf = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
+            # Compute max conf across batch for hard negative mining
+            # batch_conf : [batch_size * default_bar_num, num_classes]
+            # loss_conf : [batch_size * default_bar_num, 1]
+            batch_conf = conf_pred.view(-1, self.num_classes)
+            conf_t = torch.clamp(conf_t - 1, min=0)
+            loss_conf = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
 
-                # Hard Negative Mining
-                loss_conf = loss_conf.view(batch_size, -1)  # [batch_size, default_bar_num]
-                loss_conf[pos] = 0   # filter out pos boxes for now / positive에 해당하는 bar들을 filtering
-                _, loss_idx = loss_conf.sort(1, descending=True)    # loss_conf가 큰 idx의 내림차순 : [8, 26]
-                _, idx_rank = loss_idx.sort(1)  # 각 idx의 등수 : [batch_size, default_bar_num]
-                num_pos = pos.long().sum(1, keepdim=True)
-                num_neg = torch.clamp(self.negpos_ratio * num_pos, max=pos.size(1) - 1)     # [batch_size, 1]
-                neg = idx_rank < num_neg.expand_as(idx_rank)         # [batch_size, default_bar_num]
+            # Hard Negative Mining
+            loss_conf = loss_conf.view(batch_size, -1)  # [batch_size, default_bar_num]
+            loss_conf[pos] = 0   # filter out pos boxes for now / positive에 해당하는 bar들을 filtering
+            _, loss_idx = loss_conf.sort(1, descending=True)    # loss_conf가 큰 idx의 내림차순 : [8, 26]
+            _, idx_rank = loss_idx.sort(1)  # 각 idx의 등수 : [batch_size, default_bar_num]
+            num_pos = pos.long().sum(1, keepdim=True)
+            num_neg = torch.clamp(self.negpos_ratio * num_pos, max=pos.size(1) - 1)     # [batch_size, 1]
+            neg = idx_rank < num_neg.expand_as(idx_rank)         # [batch_size, default_bar_num]
 
-                # Confidence Loss Including Positive and Negative Examples
-                pos_idx = pos.unsqueeze(2).expand_as(conf_pred)
-                neg_idx = neg.unsqueeze(2).expand_as(conf_pred)
-                conf_p = conf_pred[(pos_idx + neg_idx).gt(0)].view(-1, self.num_classes)
-                targets_weighted = conf_t[(pos + neg).gt(0)]
-                loss_conf = self.conf_loss(conf_p, targets_weighted)
+            # Confidence Loss Including Positive and Negative Examples
+            pos_idx = pos.unsqueeze(2).expand_as(conf_pred)
+            neg_idx = neg.unsqueeze(2).expand_as(conf_pred)
+            conf_p = conf_pred[(pos_idx + neg_idx).gt(0)].view(-1, self.num_classes)
+            targets_weighted = conf_t[(pos + neg).gt(0)]
+            loss_conf = self.conf_loss(conf_p, targets_weighted)
 
-                # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
+            # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
 
-                N = num_pos.data.sum()
-                loss_loc /= N
-                loss_conf /= N
+            N = num_pos.data.sum()
+            loss_loc /= N
+            loss_conf /= N
 
-                # N_pos = num_pos.data.sum()
-                # N_neg = num_neg.data.sum()
-                # loss_loc /= N_pos
-                # loss_conf /= N_pos + N_neg
-            else:
-                pass
+            # N_pos = num_pos.data.sum()
+            # N_neg = num_neg.data.sum()
+            # loss_loc /= N_pos
+            # loss_conf /= N_pos + N_neg
 
-        alpha = 0.5
-        loss = loss_loc * alpha + loss_conf * (1. - alpha)
-
-        return loss
+        return loss_loc, loss_conf
 
 
 if __name__ == '__main__':
