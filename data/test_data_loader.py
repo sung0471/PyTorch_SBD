88ba0871e96bcd4d128687612023f87b107f14d5
SM P <zzererer0471@gmail.com>
2019-12-16 19:32:27 +0900

    191216 commit-1 - data/test_data_loader.py
    - video_loader() : video 변수에서 index error 발생하는 경우 존재 > 에러메세지 출력
    - no_candidate_frame_pos() : total_frame 및 fps 계산 부분 다른 함수로 이전
    - check_video_frames_and_fps() : 실제 사용가능한 프레임 수 계산
        → return : cv2에서 읽은 프레임 길이, 실 사용가능 프레임 길이, fps
    - make_clip_list() : 함수 제거, 해당 기능 make_dataset()으로 이동
    - make_dataset() : 여러 비디오에 대한 데이터셋 제작과정 제거
        → 후보군 추출 결과를 받아 시작 프레임 계산하는 과정 추가(Class Dataset.__get_item__()에서 이동)
    - Class Dataset
        - __init__ : 주석 제거, 실 사용가능 프레임개수를 계산하여 이 값으로 데이터셋 생성
        - __get_item__ : 후보군 추출할 경우, 프레임 위치 변수값을 조정해주는 부분을 제거

--- data/test_data_loader.py
+++ data/test_data_loader.py
@@ -44,80 +44,82 @@ def video_loader(video_path, frame_pos, sample_duration, input_type='RGB', img=F
 
             return video
     except Exception as e:
-        pass
+        assert False, 'Error msg : {}\n' \
+                      'video : {}'.format(e, video)
 
 
 def get_default_video_loader():
     return video_loader
 
 
-def no_candidate_frame_pos(root_dir, video_name, sample_duration):
+def no_candidate_frame_pos(root_dir, video_name, total_frame, sample_duration):
     video_dir = os.path.join(root_dir, video_name)
 
     frame_index_list = list()
-    # input video (cv2)
-    cap = cv2.VideoCapture(video_dir)
-    total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
     i = 0
     while i < total_frame:
         frame_index_list.append(i)
         i += sample_duration / 2
 
-    return frame_index_list, total_frame, fps
+    return frame_index_list
 
 
-def make_clip_list(video_root, video_name, sample_duration, candidate):
+def check_video_frames_and_fps(video_root, video_name):
+    # input video (cv2)
+    video_path = os.path.join(video_root, video_name)
+    cap = cv2.VideoCapture(video_path)
+    cv2_total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    total_frame = cv2_total_frames
+    while True:
+        cap.set(1, total_frame - 1)
+        status, frame = cap.read()
+        if status:
+            break
+        total_frame -= 1
+
+    return cv2_total_frames, total_frame, fps
+
+
+def make_dataset(video_root, video_name, total_frame, sample_duration, candidate):
+    video_path = os.path.join(video_root, video_name)
     video_list = list()
     if candidate:
         model = SqueezeNetFeature()
-        frame_index_list, total_frame, fps = candidate_extraction(video_root, video_name, model, adjacent=True)
-        frame_index_list = frame_index_list[1:-1]
+        frame_pos_list = candidate_extraction(video_root, video_name, total_frame, model, adjacent=True)
+        frame_index_list = list()
+        for frame_pos in frame_pos_list[1:-1]:
+            interpolated_frame_pos = int(frame_pos - (sample_duration / 2 - 1))
+            interpolated_frame_pos = 0 if interpolated_frame_pos < 0 else interpolated_frame_pos
+            frame_index_list.append(interpolated_frame_pos)
     else:
-        frame_index_list, total_frame, fps = no_candidate_frame_pos(video_root, video_name, sample_duration)
+        frame_index_list = no_candidate_frame_pos(video_root, video_name, total_frame, sample_duration)
         frame_index_list = frame_index_list[:]
 
-    video_path = os.path.join(video_root, video_name)
     for frame_pos in frame_index_list:
         info = {"video_path": video_path, "frame_pos": frame_pos, "sample_duration": sample_duration}
         video_list.append(info)
 
-    # json_path = os.path.join(video_root, os.path.splitext(video_name)[0] + ".json")
-    # json.dump(video_list, open(json_path, 'w'), indent=2)
-
-    return video_list, total_frame, fps
-
-
-def make_dataset(video_root, video_name_list, sample_duration, candidate):
-    if not isinstance(video_name_list, list):
-        video_list, total_frame, fps = make_clip_list(video_root, video_name_list, sample_duration, candidate)
-        return video_list, total_frame, fps
-    else:
-        video_all_list = list()
-        video_length_list = list()
-        video_fps_list = list()
-        for video_name in video_name_list:
-            video_list, total_frame, fps = make_clip_list(video_root, video_name, sample_duration, candidate)
-            video_all_list += video_list
-            video_length_list += [total_frame]
-            video_fps_list += [fps]
-
-        return video_all_list, video_length_list, video_fps_list
+    return video_list
 
 
 class DataSet(data.Dataset):
-    def __init__(self, video_root, video_name,
+    def __init__(self, video_root, video_name, gt_total_frame,
                  spatial_transform=None, temporal_transform=None, target_transform=None,
                  sample_duration=16, input_type='RGB', candidate=False,
                  get_loader=get_default_video_loader):
-        # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-        self.video_list, self.total_frame, self.fps = make_dataset(
-            video_root, video_name, sample_duration, candidate)
-        self.input_type = input_type
+        cv2_total_frame, total_frame, fps = check_video_frames_and_fps(video_root, video_name)
+        self.video_list = make_dataset(video_root, video_name, sample_duration, total_frame, candidate)
+        print('Video length info : cv2({}), gt({}), real_data({}), lastF({})'.format(cv2_total_frame, gt_total_frame,
+                                                                                     total_frame,
+                                                                                     self.video_list[-1]['frame_pos']))
+        self.total_frame = total_frame
+        self.fps = fps
         self.spatial_transform = spatial_transform
         self.temporal_transform = temporal_transform
         self.target_transform = target_transform
-        self.candidate = candidate
+        self.input_type = input_type
         self.loader = get_loader()
 
     def __getitem__(self, index):
@@ -132,13 +134,7 @@ class DataSet(data.Dataset):
         frame_pos = self.video_list[index]['frame_pos']
         sample_duration = self.video_list[index]['sample_duration']
 
-        if self.candidate:
-            start_frame = int(frame_pos - (sample_duration / 2 - 1))
-            start_frame = 0 if start_frame < 0 else start_frame
-        else:
-            start_frame = frame_pos
-
-        clip = self.loader(video_path, start_frame, sample_duration, self.input_type)
+        clip = self.loader(video_path, frame_pos, sample_duration, self.input_type)
         # raw_clip=clip
 
         if self.spatial_transform is not None:
@@ -148,7 +144,7 @@ class DataSet(data.Dataset):
             clip = [self.spatial_transform(img) for img in clip]
         clip = torch.stack(clip, 0).permute(1, 0, 2, 3)
 
-        return clip, start_frame
+        return clip, frame_pos
 
     def __len__(self):
         return len(self.video_list)
