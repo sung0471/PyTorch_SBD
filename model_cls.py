12c0ae4441c960c94ffbe896562870a71d96709e
psm <zzererer@naver.com>
2020-02-01 23:44:30 +0900

    200201 commit-1 - model_cls.py
    - generate_model()
        → use_extra_layer 체크하는 부분 > 해당 option이 opt 변수에 있는지 체크하는 조건 추가
    - 전체적으로 주석 추가 및 제거

--- model_cls.py
+++ model_cls.py
@@ -8,10 +8,8 @@ import os
 import copy
 
 
-# 19.6.26.
-# add parameter=model_type instead opt.model
-# 19.7.16.
-# moved from models/__init__.py
+# 19.6.26. add parameter=model_type instead opt.model
+# 19.7.16. moved from models/__init__.py
 def generate_model(opt, model_type):
     assert model_type in ['resnet', 'alexnet', 'resnext', 'detector']
 
@@ -38,7 +36,7 @@ def generate_model(opt, model_type):
     test_model = copy.deepcopy(model).to(opt.device)
 
     for_test_tensor = torch.randn(opt.batch_size, 3, opt.sample_duration, opt.sample_size, opt.sample_size).to(opt.device)
-    if not opt.use_extra_layer:
+    if 'use_extra_layer' not in vars(opt).keys() or not opt.use_extra_layer:
         flops, params = profile(test_model, inputs=(for_test_tensor,))
     else:
         start_boundaries = torch.zeros(opt.batch_size).to(opt.device)
@@ -50,8 +48,7 @@ def generate_model(opt, model_type):
     return model
 
 
-# 19.6.26.
-# add parameter=model_type
+# 19.6.26. add parameter=model_type
 # for knowledge distillation
 def build_model(opt, model_type, phase):
     if phase != "test" and phase != "train":
@@ -63,8 +60,11 @@ def build_model(opt, model_type, phase):
     model = generate_model(opt, model_type)
 
     # model=gradual_cls(opt.sample_duration,opt.sample_size,opt.sample_size,model,num_classes)
-    # print(model)
+    # 19.5.15 if not opt.no_pretrained_model 추가
+    # 19.5.23 opt.no_pretrained_model > opt.pretrained_model로 변경
+    # 19.8.5 train에서만 사전학습 모델을 불러오도록 수정
     if phase == 'train' and opt.pretrained_model:
+        # 19.8.2 pretrained_path 설정하는 부분 추가
         pretrained_model_type = model_type if model_type not in ['detector'] else opt.baseline_model
         pretrained_model_name = pretrained_model_type + '-' + str(opt.model_depth) + '-kinetics.pth'
         pretrained_path = os.path.join(opt.pretrained_dir, pretrained_model_name)
@@ -76,59 +76,59 @@ def build_model(opt, model_type, phase):
     else:
         print("no pretrained model")
 
-    # `19.3.8
-    # model = model.cuda(device)
-
-    # `19.6.4
-    # remove opt.model_type > 'new' is not trainable
-    # 19.6.26.
-    # opt.model_type = 'new' 다시 사용
-    # benchmark도 전체적으로 적용하여 재시도
-    # 19.6.28. remove opt.model_type
+    # 19.4.17 model > cuda > parallel > train 순서
+    # 19.5.15 model > benchmark > cuda > parallel > train 순서 (model_cls.py/build_model())
+    # 19.5.16 model > train > parallel > benchmark > cuda 순서 (main_baseline.py/train_dataset)
+    # 19.5.16 benchmark > model > cuda > parallel > train 순서 (model_cls.py/build_model())
+    # 19.5.20 opt.model_type == 'old' 조건 추가
+    # if opt.no_cuda and opt.model_type == 'old':
+    # model_type=='new', benchmark > model > train > parallel > cuda 순서 (main_baseline.py/build_final_model())
+    # model_type=='old', benchmark > model > cuda > parallel > train 순서 (model_cls.py/build_model())
+    # 19.5.23 opt.no_cuda > opt.cuda로 변경
+    # 19.5.30(부정확) benchmark > model > parallel > cuda > train 순서 (model_cls.py/build_model())
+    # 19.6.4 remove opt.model_type == 'old' : 'new' is not trainable
+    # 19.6.24 model > benchmark > parallel > cuda > train 순서 (model_cls.py/build_model())
+    # 19.6.26. opt.model_type == 'new' 다시 사용
+    # benchmark도 전체적으로 적용하여 재시도 (model_type=new일 때, main_baseline.py/build_final_model())
+    # model_type=='new', model > train > benchmark > parallel > cuda (main_baseline.py/build_final_model())
+    # model_type=='old', model > benchmark > parallel > cuda > train 순서 (model_cls.py/build_model())
+    # 19.6.28. remove opt.model_type == 'old'
     # if opt.cuda and opt.model_type == 'old':
     if opt.cuda:
-        # `19.6.24. 다시 추가
-        # `19.7.1. 주석 처리
-        # 19.7.10. benchmark=modelwise 재사용
+        # 19.5.15 benchmark 추가(model.to(device) 위에)
+        # 19.5.16 main_baseline.py/train_dataset() > main()으로 이동
+        # 19.6.24 다시 추가 (model_cls.py/build_model())
+        # 19.6.26 model generate 후에 적용되게 수정 (model_type=old일 때, main_baseline.py/main())
+        # 19.7.1 main_baseline.py/main()으로 이동
+        # 19.7.10 benchmark=modelwise 방식으로 다시 사용 (model_cls.py/build_model())
         torch.backends.benchmark = True
 
-        # `19.??
-        # Parallel > model.to(device) 순서로 설정
-        # `19.5.14.
-        # use multi_gpu for training and testing
+        # 원본 phase='train'과 phase='test'에 개별로 동작
+        # 19.5.14 use multi-gpu for training and testing
+        # test의 device_ids=range(1) > device_ids=range(opt.gpu_num)로 수정
+        # 19.5.15 if phase == 'train' 위로 이동(개별로 동작하던 것을 통합)
+        # 19.5.16 main_baseline.py/main()으로 이동 / 다시 롤백
+        # 19.5.20 if opt.no_cuda에서 실행되게 변경 (model_type=old일 때 조건이므로, 기능은 변화없음)
+        # 19.5.30(정확한 날짜X) Parallel > model.to(device) 순서로 변경
         model = nn.DataParallel(model, device_ids=range(opt.gpu_num))
-        # `19.7.2.
-        # model.to(device) > model = model.to(device)로 변경
-        # `19.7.7.
-        # model = model.to(device)를 main_baseline.py로 이동
-        # 19.7.10.
-        # model.inplace를 다시 modelwise로 rollback
-        # model.cuda()
-        # model.to(device)
+        # origin    model = model.cuda()
+        # 19.3.8    model.to(device)
+        # 19.4.17   model = model.cuda(device)
+        # 19.5.15 코드 수정 및 benchmark 추가
+        #           model.to(device)
+        # 19.5.16 main()으로 이동 / 다시 롤백
+        # 19.5.16   model = model.to(device)
+        # 19.5.30(정확한 날짜X) Parallel > model = model.to(device) 순서로 변경
+        # 19.5.31   model.to(device)
+        # 19.7.2    model = model.to(device)
+        # 19.7.7 main_baseline.py/main으로 이동
+        # 19.7.10 model.inplace를 다시 modelwise로 rollback
+        # 19.10.18 device > opt.device
         model = model.to(opt.device)
 
-        # `19.6.27.
-        # use model.to(device)>Parallel 순서로 설정
-        # `19.7.2. 주석처리
-        # # model.cuda()
-        # # model = model.to(device)
-        # model.to(device)
-        # # `19.5.14.
-        # # use multi_gpu for training and testing
-        # model = nn.DataParallel(model, device_ids=range(opt.gpu_num))
-
     if phase == 'train':
-        # for debug
-        # print(opt.gpu_num)
-
-        # `19.3.8
-        # I'll use only one gpu
-        # model = nn.DataParallel(model, device_ids=range(1))
-        # use multi gpu for only training
-        # model = nn.DataParallel(model, device_ids=range(opt.gpu_num))
         model.train()
     else:
-        # model = nn.DataParallel(model, device_ids=range(1))
         model.eval()
 
     return model
